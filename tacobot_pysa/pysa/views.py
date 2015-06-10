c0eba3a65a469c56d0459d0b27f7cbf8d71be97f
Alejandro Sáenz <asaenzestrada@gmail.com>
2015-06-10 15:12:10 -0700

    + Django project

--- tacobot_pysa/pysa/views.py
+++ tacobot_pysa/pysa/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.template.context import RequestContext
+
 
 # Create your views here.
+def system(request):
+    obj =  dict()
+    obj['as'] = 'asdasd'
+    obj['asfasd'] = 1
+    return render_to_response('index.html', obj, context_instance=RequestContext(request))
