ba28d8abc7ecb31c30b6ba1dbda50663e91185d6
Alejandro Sáenz <asaenzestrada@gmail.com>
2015-06-11 06:57:55 -0700

    + taco-template + POST handler

--- tacobot_pysa/pysa/views.py
+++ tacobot_pysa/pysa/views.py
@@ -4,7 +4,10 @@ from django.template.context import RequestContext
 
 # Create your views here.
 def system(request):
-    obj =  dict()
-    obj['as'] = 'asdasd'
-    obj['asfasd'] = 1
-    return render_to_response('index.html', obj, context_instance=RequestContext(request))
+    if request.POST:
+        return True
+    else:
+        obj =  dict()
+        obj['as'] = 'asdasd'
+        obj['asfasd'] = 1
+        return render_to_response('index.html', obj, context_instance=RequestContext(request))
--- tacobot_pysa/tacobot_pysa/settings.py
+++ tacobot_pysa/tacobot_pysa/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*5&b26em6elae@=%i&ug665$uege@nu*&k3+wf4pn3d*=f*97j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
 '127.0.0.1',
